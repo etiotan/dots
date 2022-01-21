dc756f101b72b83768f029858f034443078a5a12
Etio Tan <etiotan@gmail.com>
2022-01-21 00:46:57 -0500

    tidy emacs.el

--- .emacs.el
+++ .emacs.el
@@ -1,4 +1,4 @@
- ;; Set up package.el to work with MELPA
+;; Set up package.el to work with MELPA
 (require 'package)
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/")) (package-initialize)
@@ -14,32 +14,18 @@
  '(custom-safe-themes
    '("021321ae56a45794f43b41de09fb2bfca184e196666b7d7ff59ea97ec2114559" "4780d7ce6e5491e2c1190082f7fe0f812707fc77455616ab6f8b38e796cbffa9" "3e335d794ed3030fefd0dbd7ff2d3555e29481fe4bbb0106ea11c660d6001767" default))
  '(inhibit-startup-screen t)
- '(org-agenda-files '("~/org-roam/20220116132439-binary_search.org"))
+ '(org-agenda-files nil)
  '(org-hide-emphasis-markers t)
  '(package-selected-packages
    '(rjsx-mode smartparens org-roam-ui use-package org-roam google-this magit eink-theme)))
 
+;; Editor Settings
+(require 'smartparens-config)
+(smartparens-global-mode 1)
+(require 'recentf)
+(recentf-mode 1)
 
-(global-display-line-numbers-mode 1)
-(setq display-line-numbers-type 'relative)
-(global-visual-line-mode t)
-
-(load-theme 'eink t)
-(set-face-foreground 'font-lock-comment-face "#A9A9A9")
-
-
-;; fontify code in code blocks
-(setq org-src-fontify-natively 1)
-      
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-
-(show-paren-mode 1)
-
-;; spellchecker
-(setq ispell-program-name "/usr/local/bin/ispell")
-(add-hook 'text-mode-hook 'flyspell-mode)
-
+;; Note Taking
 (use-package org-roam
   :ensure t
   :custom
@@ -56,18 +42,27 @@
          ("C-c n d" . org-roam-dailies-goto-date))
   :config
   (org-roam-db-autosync-mode)
-  ;; If using org-roam-protocol
   (require 'org-roam-protocol))
-  ;; Journaling setup
-  (setq org-roam-dailies-directory "~/org-roam/daily")
-
-(require 'smartparens-config)
-
-(smartparens-global-mode 1)
+(setq org-roam-dailies-directory "~/org-roam/daily")
 
 (add-to-list 'load-path "~/.emacs.d/private/org-roam-ui")
 (load-library "org-roam-ui")
 
+(setq ispell-program-name "/usr/local/bin/ispell")
+(add-hook 'text-mode-hook 'flyspell-mode)
+
+
+;; Visual Settings
+(show-paren-mode 1)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(global-display-line-numbers-mode 1)
+(setq display-line-numbers-type 'relative)
+(global-visual-line-mode t)
+(setq org-src-fontify-natively 1)
+
+(load-theme 'eink t)
+(set-face-foreground 'font-lock-comment-face "#A9A9A9")
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
