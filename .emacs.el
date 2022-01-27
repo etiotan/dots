a9596545281d316887eb67df1aaa95164513a1af
Etio Tan <etiotan@gmail.com>
2022-01-27 00:37:30 -0500

    google this hotkey

--- .emacs.el
+++ .emacs.el
@@ -4,6 +4,61 @@
 	     '("melpa" . "https://melpa.org/packages/")) (package-initialize)
 (package-refresh-contents)
 
+;; Editor Settings
+(require 'smartparens-config)
+(smartparens-global-mode 1)
+(require 'recentf)
+(recentf-mode 1)
+(global-set-key (kbd "C-c C-g") 'google-this)
+
+;; Note Taking
+(use-package org-roam
+  :ensure t
+  :custom
+  (org-roam-directory (file-truename "~/Documents/org-roam"))
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n g" . org-roam-ui-mode)
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n c" . org-roam-capture)
+         ;; Dailies
+         ("C-c n t" . org-roam-dailies-capture-tomorrow)
+	 ("C-c n [" . org-roam-dailies-goto-previous-note)
+	 ("C-c n ]" . org-roam-dailies-goto-next-note)
+         ("C-c n d" . org-roam-dailies-goto-date))
+  :config
+  (org-roam-db-autosync-mode)
+  (require 'org-roam-protocol))
+(setq org-roam-dailies-directory "~/Documents/org-roam/daily")
+
+(add-to-list 'load-path "~/.emacs.d/private/org-roam-ui")
+(load-library "org-roam-ui")
+
+(setq ispell-program-name "/usr/local/bin/ispell")
+(add-hook 'text-mode-hook 'flyspell-mode)
+
+;; Visual Settings
+(show-paren-mode 1)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(global-display-line-numbers-mode 1)
+(setq display-line-numbers-type 'relative)
+(global-visual-line-mode t)
+(setq org-src-fontify-natively 1)
+(load-theme 'eink t)
+(set-face-foreground 'font-lock-comment-face "#A9A9A9")
+
+;; Custom, Do not edit directly
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 180 :width normal :foundry "nil" :family "Hack"))))
+ '(org-block ((t (:background "#EFF0F1" :extend t))))
+ '(org-block-begin-line ((t (:underline "#A7A6AA" :foreground "#008ED1" :background "#EAEAFF" :extend t))))
+ '(org-block-end-line ((t (:overline "#A7A6AA" :foreground "#008ED1" :background "#EAEAFF" :extend t)))))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -18,57 +73,3 @@
  '(org-hide-emphasis-markers t)
  '(package-selected-packages
    '(rjsx-mode smartparens org-roam-ui use-package org-roam google-this magit eink-theme)))
-
-;; Editor Settings
-(require 'smartparens-config)
-(smartparens-global-mode 1)
-(require 'recentf)
-(recentf-mode 1)
-
-;; Note Taking
-(use-package org-roam
-  :ensure t
-  :custom
-  (org-roam-directory (file-truename "~/org-roam"))
-  :bind (("C-c n l" . org-roam-buffer-toggle)
-         ("C-c n f" . org-roam-node-find)
-         ("C-c n g" . org-roam-ui-mode)
-         ("C-c n i" . org-roam-node-insert)
-         ("C-c n c" . org-roam-capture)
-         ;; Dailies
-         ("C-c n t" . org-roam-dailies-capture-tomorrow)
-	 ("C-c n [" . org-roam-dailies-goto-previous-note)
-	 ("C-c n ]" . org-roam-dailies-goto-next-note)
-         ("C-c n d" . org-roam-dailies-goto-date))
-  :config
-  (org-roam-db-autosync-mode)
-  (require 'org-roam-protocol))
-(setq org-roam-dailies-directory "~/org-roam/daily")
-
-(add-to-list 'load-path "~/.emacs.d/private/org-roam-ui")
-(load-library "org-roam-ui")
-
-(setq ispell-program-name "/usr/local/bin/ispell")
-(add-hook 'text-mode-hook 'flyspell-mode)
-
-
-;; Visual Settings
-(show-paren-mode 1)
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(global-display-line-numbers-mode 1)
-(setq display-line-numbers-type 'relative)
-(global-visual-line-mode t)
-(setq org-src-fontify-natively 1)
-
-(load-theme 'eink t)
-(set-face-foreground 'font-lock-comment-face "#A9A9A9")
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 180 :width normal :foundry "nil" :family "Hack"))))
- '(org-block ((t (:background "#EFF0F1" :extend t))))
- '(org-block-begin-line ((t (:underline "#A7A6AA" :foreground "#008ED1" :background "#EAEAFF" :extend t))))
- '(org-block-end-line ((t (:overline "#A7A6AA" :foreground "#008ED1" :background "#EAEAFF" :extend t)))))
--- copy_dots.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cp ~/.zshrc ~/.dots/
-cp ~/.vimrc ~/.dots/
-cp ~/.emacs.el ~/.dots/
-cp -R ~/.vim/init ~/.dots/
-
-echo "Dot files copied"
