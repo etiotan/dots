a5308f3decd4f15bad2f7db4f490c508ff2b7f2b
Etio Tan <etiotan@gmail.com>
2022-02-26 00:54:36 -0500

    Tidy emacs

--- .emacs.el
+++ .emacs.el
@@ -9,8 +9,8 @@
 (smartparens-global-mode 1)
 (require 'recentf)
 (recentf-mode 1)
-(global-set-key (kbd "C-c C-g") 'google-this)
-(global-set-key (kbd "C-c C-r") 'recentf-open-files)
+(global-set-key (kbd "C-c g") 'google-this)
+(global-set-key (kbd "C-c r") 'recentf-open-files)
 (setq history-length 17)
 (savehist-mode 1)
 (save-place-mode 1)
@@ -19,7 +19,7 @@
 (require 'prettier-js)
 (add-to-list 'auto-mode-alist '("components\\/.*\\.js\\'" . rjsx-mode))
 
-;; Note Taking
+;; Note Taking / Org Roam
 (use-package org-roam
   :ensure t
   :custom
@@ -38,12 +38,12 @@
   (org-roam-db-autosync-mode)
   (require 'org-roam-protocol))
 (setq org-roam-dailies-directory "~/Documents/org-roam/daily")
-
 (add-to-list 'load-path "~/.emacs.d/private/org-roam-ui")
 (load-library "org-roam-ui")
-
 (setq ispell-program-name "/usr/local/bin/ispell")
 (add-hook 'text-mode-hook 'flyspell-mode)
+
+
 
 ;; Visual Settings
 (show-paren-mode 1)
@@ -55,29 +55,5 @@
 (setq org-src-fontify-natively 1)
 (load-theme 'eink t)
 (set-face-foreground 'font-lock-comment-face "#A9A9A9")
-
-;; Custom, Do not edit directly
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 180 :width normal :foundry "nil" :family "Hack"))))
- '(org-block ((t (:background "#EFF0F1" :extend t))))
- '(org-block-begin-line ((t (:underline "#A7A6AA" :foreground "#008ED1" :background "#EAEAFF" :extend t))))
- '(org-block-end-line ((t (:overline "#A7A6AA" :foreground "#008ED1" :background "#EAEAFF" :extend t)))))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(c-basic-offset 4)
- '(custom-enabled-themes '(eink))
- '(custom-safe-themes
-   '("021321ae56a45794f43b41de09fb2bfca184e196666b7d7ff59ea97ec2114559" "4780d7ce6e5491e2c1190082f7fe0f812707fc77455616ab6f8b38e796cbffa9" "3e335d794ed3030fefd0dbd7ff2d3555e29481fe4bbb0106ea11c660d6001767" default))
- '(inhibit-startup-screen t)
- '(org-agenda-files nil)
- '(org-hide-emphasis-markers t)
- '(package-selected-packages
-   '(rjsx-mode smartparens org-roam-ui use-package org-roam google-this magit eink-theme)))
+(set-face-font 'default "-*-Hack-normal-normal-normal-*-*-*-*-*-m-0-iso10646-1")
+(set-face-attribute 'default (selected-frame) :height 200)
